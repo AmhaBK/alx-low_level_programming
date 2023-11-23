923978aa1ce5386f25e24ee663e33e09e68bc2f0
AmhaBK <amhabink@gmail.com>
2023-11-23 14:38:40 -0800

    dbdbg

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * clear_bit This prog sets the value of a bit to 0 at a given index
+ * * clear_bit - This prog sets the value of a bit to 0 at a given index
  * * @n: parameter
  * * @index: index
  * * Return: 1 if success, -1 if error
